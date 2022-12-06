686e881cf90cac423fc4fdc3293c090cad64dd49
SSung023 <50323157+SSung023@users.noreply.github.com>
2022-12-06 16:29:52 +0900

    [Feat] Create project for SangCom
    1. Java Spring과 React 연동 프로젝트 업데이트
    2. ./gradlew build를 통해 빌드 완료

---
+++ src/main/java/Project/SangCom/SangComApplication.java
@@ -0,0 +1,13 @@
+package Project.SangCom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangComApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Project/SangCom/SangComApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.SangCom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
