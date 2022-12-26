11dd6f686a5e857041c427b1fb4d014b26cc1f51
Kim Da Eun <midekuna@gmail.com>
2022-12-27 05:17:16 +0900

    [Feat] Add elapsed time feature at BoardPreview
    BoardPreview.jsx, BoardPreview.css
    1. 게시글의 작성 경과 시간을 보여주는 함수 추가
    2. 스타일 수정

--- src/main/frontend/src/components/BoardPreview.jsx
+++ src/main/frontend/src/components/BoardPreview.jsx
@@ -1,32 +1,54 @@
 import React from 'react';
 import { useState } from 'react';
 import './BoardPreview.css';
-//import Board from '../mock/board.json';
-import BoardEmpty from '../mock/boardFake.json';
+import Board from '../mock/board.json';
+//import BoardEmpty from '../mock/boardFake.json';
 
-// board 종류별로 key가 있다고 가정. 전달받은 data(배열 형식)에서 id 별로 보여주는 내용을 달리함
-// axios를 사용해 데이터를 받아오면 JSON 형식임. 이걸 가공해서 처리하기 위해 배열로 바꾸는 함수 필요
-export default function BoardPreview({ boardKey }) {
-    const db = Object.values(BoardEmpty); //JSON 파일을 배열로 변환
-    const [isEmpty, setIsEmpty] = useState(db.lenth === 0);
-
-    return (
-        <div className='preview'>
-            {boardKey === 0 && <div className='title'>자유게시판</div>}
-            {boardKey === 1 && <div className='title'>학생회 공지</div>}
-        </div>
-    );
-    !isEmpty && db[boardKey].map((item) => {
-        return (
-            <div className='board-preview'>
-                
-            </div>
-        );
-    });
-    return (
-        <div className='board-empty'>
-            <p>보드가 비어있습니다.</p>
-        </div>
-    );   
+function MakeTitle({ boardKey }){
+    return boardKey === 0 ? <div className='title'>자유게시판</div> : <div className='title'>학생회 공지</div>
 }
 
+// board 종류별로 key가 있다고 가정. 
+// 전달받은 data(배열 형식)에서 id 별로 보여주는 내용을 달리함
+export default function BoardPreview({ boardKey }) {
+    const [db, setDb] = useState(Object.values(Board)); // JSON 파일을 배열로 변환
+    const getTime = (time) => {                         // 작성 경과 시간을 구하는 함수
+        let timeTxt = `방금 전`;
+        const curTime = new Date();                     
+        const diff = (curTime.getTime() - (new Date(time)).getTime()) / 1000; // Date 연산의 단위는 millisecond
+        
+        const times = [
+            { name: '년', milliSeconds: 60 * 60 * 24 * 365 },
+            { name: '개월', milliSeconds: 60 * 60 * 24 * 30 },
+            { name: '일', milliSeconds: 60 * 60 * 24 },
+            { name: '시간', milliSeconds: 60 * 60 },
+            { name: '분', milliSeconds: 60 },
+        ];
+        
+        for(const value of times){
+            const betweenTime = Math.floor(diff / value.milliSeconds);
+        
+            if (betweenTime > 0) {
+                timeTxt = `${betweenTime}${value.name} 전`;
+                return timeTxt;
+            }
+        }
+        return timeTxt;
+    }
+    
+    return (
+        <div className='preview'>
+            <MakeTitle boardKey={boardKey} />
+            <div className='board'>
+                {db[boardKey].map((item) => (
+                    <div key={item.id} className='list'>
+                        <a href="/board">
+                            <p className='board-title'>{item.title}</p>
+                            <p className='time'>{`${getTime(item.date)}`}</p>
+                        </a>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
