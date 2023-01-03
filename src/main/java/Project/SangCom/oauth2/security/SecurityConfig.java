e10397efd98218e01a1aa8c168412ec76a51f17f
SSung023 <50323157+SSung023@users.noreply.github.com>
2023-01-03 23:15:40 +0900

    [Fix] Fix CORS problem

--- src/main/java/Project/SangCom/oauth2/security/SecurityConfig.java
+++ src/main/java/Project/SangCom/oauth2/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,6 +30,7 @@ public class SecurityConfig {
 
         http.authorizeRequests()
                 .antMatchers("/oauth2/**").permitAll()
+//                .antMatchers(HttpMethod.OPTIONS).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .logout()
