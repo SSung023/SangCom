a81b2a2f6390d7679342d5b06aee893b70920067
Kim Da Eun <midekuna@gmail.com>
2023-01-18 00:18:21 +0900

    [Feat] Store access token and user informations in Redux store
    1. 존재하는 회원이 로그인을 시도할 때 jwt access token을 state로 저장
    2. access token을 이용해 유저 정보를 받아오는 GET 요청 수행

--- src/main/frontend/src/pages/Auth.jsx
+++ src/main/frontend/src/pages/Auth.jsx
@@ -1,55 +1,71 @@
 import React from 'react';
 import { useEffect } from 'react';
-import { useState } from 'react';
-import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setTokenAction } from '../reducers/jwtReducer';
 import axios from 'axios';
+import { loginAction } from '../reducers/loginReducer';
 
 // BE로부터 access token을 axios 통신으로 받아와 store에 저장
-// 
-// 
 export default function Auth() {
-    // Redux 사용
-    const dispatch = useDispatch();
-    const handleToken = (token) => () => {
-        dispatch(setTokenAction(token));
-    };
-
-    // 내부적으로 token을 관리하기 위한 state
-    const [ accessToken, setAccessToken ] = useState("");
+    // 쿼리에서 email 정보 받아 오기
     const email = new URL(window.location.href).searchParams.get("email");
 
-    const token = useSelector(state => state.token, shallowEqual);
-    
-    const axios_post = () => {
-        const url = "/api/auth/token";
-        const data = {
-            email: email
-        }
+    // Redux 사용
+    const dispatch = useDispatch();
+    const actoken = useSelector((state) => state.loginReducer.user.access_token);
 
-        axios.post(url, data)
+    const axios_post = () => {
+        const tokenUrl = "/api/auth/token";
+
+        axios.post(tokenUrl, {
+            email: email
+        })
         .then(function (res) {
-            console.log(res.headers.get('Authorization'));
-            // 내부 state에 토큰 업데이트
-            //setAccessToken(res.headers.get('Authorization'));
+            const accessToken = res.headers.get('Authorization');
             // Dispatch에 토큰 전달
-            //handleToken(accessToken);
+            dispatch(setTokenAction(accessToken));
         })
-        .catch(function (error) {
-            console.log(error);
-        })
-        .then(function (res) {
-            console.log(res);
+        .catch(function (e) {
+            console.log(e);
         })
     };
+
+    
+    // TODO access token과 user의 정보를 통신이 모두 끝나고 성공했을 때 저장하는 방식으로 바꾸기
+    
+    useEffect(axios_post, []);
+    useEffect(() => {
+        const userUrl = "/api/auth/user";
+        const dummyUserInfos = {
+            role: "STUDENT",
+            username: "김다은",
+            nickname: "단두대",
+            grade: "1",
+            classes: "7",
+            number: "2",
+            chargeSubject: "",
+            chargeGrade: ""
+        };
+
+        if(actoken) {
+            axios.get(userUrl, {
+                headers: {
+                    Authorization: `${actoken}`,
+                }
+            })
+            .then(function (res) {
+                console.log(res.data);
+                dispatch(loginAction(dummyUserInfos));
+                // TODO: main page redirect part
+            })
+            .catch(function (e) {
+                console.log(e);
+            })
+        }
+    }, [actoken]);
 
     return (
         <div>
-            {
-                useEffect(axios_post, [])
-            }
-            {/* Store에 있는 토큰 출력하기 */}
-            {token}
         </div>
     );
 }
--- src/main/frontend/src/reducers/jwtReducer.jsx
+++ src/main/frontend/src/reducers/jwtReducer.jsx
@@ -1,17 +1,22 @@
 export const SET_TOKEN = "jwt/SET_TOKEN";
+export const DELETE_TOKEN = "jwt/DELETE_TOKEN";
 
 export const setTokenAction = (token) => ({ type: SET_TOKEN, token });
 
-const initialState = ""; // NULL
+const initialState = {
+    access_token: ""
+};
 
 // jwt accesstoken을 관리하는 reducer 
-const loginReducer = ( state = initialState, action ) => {
+const jwtReducer = ( state = initialState, action ) => {
     switch(action.type) {
         case SET_TOKEN:
-            return action.token ? action.token : "토큰이 유효하지 않습니다.";
+            return {...state, access_token: `${action.token}`};
+        case DELETE_TOKEN:
+            return {...state, access_token: ""};
         default:
             return state;
     }
 };
 
-export default loginReducer;
+export default jwtReducer;
