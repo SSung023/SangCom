2ef19833c2dfd33d24ee51bf87a2d0fb1500bcd4
Kim Da Eun <midekuna@gmail.com>
2023-03-05 23:58:17 +0900

    [Feat] Reveal LNBs hover GNB
    1. GNB 메뉴에 마우스가 오버되면 LNB가 노출되도록 구현
    2. 라우팅 - PrivateRoute 시도했지만 실패...

--- src/main/frontend/src/components/Mainmenu.js
+++ src/main/frontend/src/components/Mainmenu.js
@@ -1,115 +1,110 @@
-import React, {Component} from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import styles from './Topnav.module.css';
 
-class Mainmenu extends Component{
-    render() {
-        const mainMenus = {
-            "게시판": [
-                { lnbMenu: '자유 게시판', category: 'free', to: 'board/free' },
-                { lnbMenu: '1학년 게시판', category: 'grade1', to: 'board/grade1' },
-                { lnbMenu: '2학년 게시판', category: 'grade2', to: 'board/grade2' },
-                { lnbMenu: '3학년 게시판', category: 'grade3', to: 'board/grade3' }
-            ],
-            "학생회": [
-                { lnbMenu: "학생회 공지 게시판", category: 'council', to: 'board/council' },
-                { lnbMenu: "학생회 건의 게시판", category: 'suggestion', to: 'board/suggestion' }
-            ],
-            "동아리": [
-                { lnbMenu: "동아리 게시판", category: 'club', to: 'board/club' }
-            ],
-            "시간표": [
-                { to: 'timetable' }
-            ],
-            "급식표": [
-                { to: 'mealPage' }
-            ]
-        }
+export default function Mainmenu() {
+    const role = useSelector((state) => state.loginReducer.user.info.role);
+    const grade = useSelector((state) => state.loginReducer.user.info.grade);
 
-        return (
-            <div className={styles.mainMenu}>
-                {/* <ul>
-                    <li>
-                        <NavLink 
-                            to='board/free' 
-                            style={({isActive}) => ({ color: isActive ? 'var(--blue-color)' : ''})} 
-                            className={styles.item}
-                        >게시판</NavLink>
-                    </li>
-                    <li>
-                        <NavLink 
-                            to='board/council' 
-                            style={({isActive}) => ({ color: isActive ? 'var(--blue-color)' : ''})} 
-                            className={styles.item}
-                        >학생회</NavLink>
-                    </li>
-                    <li>
-                        <NavLink 
-                            to='board/club' 
-                            style={({isActive}) => ({ color: isActive ? 'var(--blue-color)' : ''})} 
-                            className={styles.item}
-                        >동아리</NavLink>
-                    </li>
-                    <li>
-                        <NavLink 
-                            to='timetable' 
-                            style={({isActive}) => ({ color: isActive ? 'var(--blue-color)' : ''})} 
-                            className={styles.item}
-                        >시간표</NavLink>
-                    </li>
-                    <li>
-                        <NavLink 
-                            to='mealPage' 
-                            className={styles.item} 
-                            style={({isActive}) => ({ color: isActive ? 'var(--blue-color)' : ''})} 
-                        >급식표</NavLink>
-                    </li>
-                </ul> */}
-                <ul>
-                    {Object.keys(mainMenus).map((mainMenu) => {
-                        const lnbMenus = mainMenus[mainMenu];
-
-                        return (
-                            lnbMenus.length === 1 ?   
-                            <li key={mainMenu} className={styles.item}>
-                                {lnbMenus.map((menu) => {
-                                    return (
-                                    <NavLink 
-                                        to={menu.to}
-                                        style={({isActive}) => ({ color: isActive ? 'var(--blue-color)' : ''})}
-                                        key={mainMenu}
-                                    >{mainMenu}</NavLink>);
-                                })}
-                            </li> :   
-                            <li key={mainMenu} className={styles.item}>
-                                <LNB lnbMenus={lnbMenus} mainMenu={mainMenu}/>                                    
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-        );
+    const mainMenus = {
+        "게시판": [
+            { lnbMenu: '자유 게시판', category: 'free', to: 'board/free' },
+            { lnbMenu: '학년별 게시판', category: 'grade1', to: 'board/grade1' },
+            { lnbMenu: '학년별 게시판', category: 'grade2', to: 'board/grade2' },
+            { lnbMenu: '학년별 게시판', category: 'grade3', to: 'board/grade3' }
+        ],
+        "학생회": [
+            { lnbMenu: "학생회 공지 게시판", category: 'council', to: 'board/council' },
+            { lnbMenu: "학생회 건의 게시판", category: 'suggestion', to: 'board/suggestion' }
+        ],
+        "동아리": [
+            { lnbMenu: "동아리 게시판", category: 'club', to: 'board/club' }
+        ],
+        "시간표": [
+            { to: 'timetable' }
+        ],
+        "급식표": [
+            { to: 'mealPage' }
+        ]
     }
-}
 
-function LNB({ lnbMenus, mainMenu }) {
     return (
-        <>
-            <div>
-                {mainMenu}
-            </div>
-            {/* <NavLink 
-                to={lnbMenus.to} 
-                style={({isActive}) => ({ color: isActive ? 'var(--blue-color)' : ''})}
-            >{mainMenu}
-            </NavLink> */}
-            <div className={styles.lnbMenu}>
-                {lnbMenus.map((lnbMenu) => {
-                    // console.log(lnbMenu);
+        <div className={styles.mainMenu}>
+            <ul className={styles.menuUl}>
+                {Object.keys(mainMenus).map((mainMenu) => {
+                    const lnbMenus = mainMenus[mainMenu];
+
+                    if(mainMenu === "게시판" && role === "TEACHER"){
+                        return;
+                    }
+
+                    return (
+                        lnbMenus.length === 1 ?   
+                        <li key={mainMenu} className={styles.item}>
+                            {lnbMenus.map((menu) => {
+                                return (
+                                <NavLink 
+                                    to={menu.to}
+                                    style={({isActive}) => ({ color: isActive ? 'var(--blue-color)' : ''})}
+                                    key={mainMenu}
+                                >{mainMenu}</NavLink>);
+                            })}
+                        </li> :
+                        <LNB key={mainMenu} lnbMenus={lnbMenus} mainMenu={mainMenu} grade={grade}/>
+                    );
                 })}
-            </div>
-        </>
+            </ul>
+        </div>
     );
 }
 
-export default Mainmenu;
+function LNB({ lnbMenus, mainMenu, grade }) {
+    const [isHide, setHide] = useState(true);
+    const onMouseEnter = () => { setHide(false) };
+    const onMouseLeave = () => { setHide(true) };
+
+    return (
+        <>
+            <li 
+                key={mainMenu} 
+                className={styles.item}
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+            >
+                <NavLink 
+                    to={lnbMenus[0].to} 
+                    style={({isActive}) => ({ color: isActive ? 'var(--blue-color)' : ''})}
+                >
+                {mainMenu}                                  
+                </NavLink>
+
+                {!isHide && <div className={styles.lnb}>
+                    <ul className={styles.menuUl}>
+                    {lnbMenus.map((menu) => {
+                        return (
+                            // category가 자유 게시판이 아니라면 학년에 맞는 메뉴가 표시되도록 렌더링
+                            <li key={menu.category}>
+                                {menu.lnbMenu !== "학년별 게시판" ? 
+                                <NavLink 
+                                    to={menu.to}
+                                    style={({isActive}) => ({ color: isActive ? 'var(--blue-color)' : ''})}
+                                    key={menu.category}
+                                >{menu.lnbMenu}</NavLink>
+                                : 
+                                menu.category === `grade${grade}` && 
+                                <NavLink 
+                                    to={menu.to}
+                                    style={({isActive}) => ({ color: isActive ? 'var(--blue-color)' : ''})}
+                                    key={menu.category}
+                                >{menu.lnbMenu}</NavLink>
+                                }
+                            </li>
+                        );
+                    })}
+                    </ul>
+                </div>}
+            </li>
+        </>
+    );
+}
--- src/main/frontend/src/utility/PrivateRoute.jsx
+++ src/main/frontend/src/utility/PrivateRoute.jsx
@@ -11,26 +11,36 @@ export default function PrivateRoute({ component, allowedRole }) {
     const isLogin = useLogin();
     const location = useLocation();
     const params = useParams();
-    const [isAllowed, setAllowed] = useState();
+    const [isAllowed, setAllowed] = useState(true);
     const role = useSelector((state) => state.loginReducer.user.info.role);
 
     // category가 undefined면 location으로, 
     // category가 있으면 category로 allowedRole을 검사한다.
     useEffect(() => {
         if(params.category) {
-            setAllowed(allowedRole[params.category].includes(role));
+            // console.log(`category 검사, category: ${params.category}, allowedList: ${allowedRole[params.category]}, role: ${role[0]}`);
+            // setAllowed(allowedRole[params.category].includes(role[0]));
+            setAllowed(Object.values(role).every((r) => {allowedRole[params.category].includes(r)}));
         }
         else {
-            setAllowed(allowedRole[location.pathname].includes(role));
+            // console.log(`pathname 검사, pathname: ${location.pathname}, allowedList: ${allowedRole[location.pathname]}, role: ${role[0]}`);
+            // setAllowed(allowedRole[location.pathname].includes(role[0]));
+            setAllowed(Object.values(role).every((r) => {allowedRole[location.pathname].includes(r)}));
         }
-    }, [location]);
+    }, [location, role]);
 
-    // console.log("logout please " + !isLogin);
+    const render = () => {
+        // console.log(isAllowed);
+        return (
+            isAllowed ? component : <Unauthorized />
+        );
+    }
 
     return (
-        isLogin ? 
+        isLogin ?
         component
         : <Navigate replace to='/login'/>
-        // (isAllowed ? component : <Unauthorized />) 
+        // TODO: 라우팅 고치기: role이 Root에서 초기화되기 때문에 role이 undefined가 되는 순간 PrivateRoute에서는 아무것도 할 수가 없음.
+        // role && render()
     );
 }
